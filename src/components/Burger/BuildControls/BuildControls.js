9d4b60bb811097e0bdc043017d3d44f5a32ac266
ukroma1992 <ukroma1992@gmail.com>
2018-07-12 10:44:13 +0300

    Make showing hiding the modal

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -22,7 +22,8 @@ const buildControls = (props) => (
         )) }
         <button 
             className={classes.OrderButton} 
-            disabled={!props.purchasable}>ЗАКАЗАТЬ СЕЙЧАС</button>   
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ЗАКАЗАТЬ СЕЙЧАС</button>   
     </div>
 );
 
